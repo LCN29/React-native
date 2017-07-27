53b18dbe80573df34924f36318f0e9a7ea157418
LCN29 <1455476411@qq.com>
2017-07-27 17:16:40 +0800

    ThirtstyDays 09

---
+++ ThirstyDays/View/day09/Day09.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Ape on 2017/7/20.
+ */
+
+import React,{ Component } from 'react';
+
+import TwitterUser from './TwitterUser';
+
+export default class Day09 extends Component{
+
+    render(){
+        return (
+            <TwitterUser/>
+        );
+    }
+}
