a1bc93d4307e1a08e9f1bf98c7f3dabc16979989
LCN29 <1455476411@qq.com>
2017-07-27 16:59:58 +0800

    ThirstyDays  02

--- ThirstyDays/setup.js
+++ ThirstyDays/setup.js
@@ -9,12 +9,13 @@ import {
 } from 'react-native';
 
 import Day01 from './View/day01/Day01';
+import Day02 from './View/day02/Day02';
 
 export default class setup extends Component {
 
     render() {
         return (
-            <Day01/>
+            <Day02/>
         );
     }
 }
